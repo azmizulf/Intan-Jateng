d1d7c7d41ac9f1db825da7f486a3b7d455489f74
unknown <222111940@stis.ac.id>
2024-08-22 10:41:37 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>MARTANI</title>
+	<link rel="stylesheet" href="/assets/css/stylelogin.css" />
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="/assets/img/logo.png" />
+</head>
+<body>
+	<div class="container">
+		<div class="img">
+			<img src="/assets/img/imgleft.png">
+		</div>
+		<div class="login-content">
+			<form action="/index">
+				<img src="/assets/img/logo.png">
+				<h2 class="title">Selamat Datang!</h2>
+           		<div class="input-div one">
+           		   <div class="i">
+           		   		<i class="fas fa-user"></i>
+           		   </div>
+           		   <div class="div">
+           		   		<input type="text" class="input" placeholder ="Username">
+           		   </div>
+           		</div>
+           		<div class="input-div pass">
+           		   <div class="i"> 
+           		    	<i class="fas fa-lock"></i>
+           		   </div>
+           		   <div class="div">
+           		    	<input type="password" class="input" placeholder ="Password ">
+            	   </div>
+            	</div>
+            	<input type="submit" class="btn" value="Login">
+            </form>
+        </div>
+    </div>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
